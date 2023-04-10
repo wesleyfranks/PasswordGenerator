166c3f1dcb9de4bb0906ae850fc3ed6d05cd05ca
Wesley Franks <code.wesleyfranks@gmail.com>
2023-04-09 22:45:52 -0500

    fixed bug where you click generate password when words is checked and it wouldn't generate words.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const lengthSlider = document.querySelector(".pass-length input");
+const passLength = document.querySelector(".pass-length span");
 const options = document.querySelectorAll(".option input");
 const copyIcon = document.querySelector(".input-box span");
 const passwordInput = document.querySelector(".input-box input");
@@ -36,56 +37,6 @@ const getWords = async (numWords) => {
 
 const generatePassword = async () => {
   console.log("----------generatePassword function ran------------");
-  let staticPassword = "";
-  let randomPassword = "";
-  let passLength = lengthSlider.value;
-
-  for (const option of options) {
-    if (option.checked) {
-      if (option.id === "words") {
-        const numWords = lengthSlider.value;
-        const words = await getWords(numWords);
-        staticPassword += words.join("");
-        passLength = words.length;
-      } else if (option.id !== "exc-duplicate") {
-        staticPassword += characters[option.id];
-      } else {
-        excludeDuplicate = true;
-      }
-    }
-  }
-
-  if (staticPassword !== "") {
-    for (let i = 0; i < passLength; i++) {
-      let randomChar =
-        staticPassword[Math.floor(Math.random() * staticPassword.length)];
-      if (excludeDuplicate && randomPassword.includes(randomChar)) {
-        i--;
-      } else {
-        randomPassword += randomChar;
-      }
-    }
-    passwordInput.value = randomPassword;
-  }
-
-  console.log("passwordInput Length = " + passwordInput.value.length);
-};
-
-
-
-const updatePassIndicator = () => {
-    passIndicator.id =
-        lengthSlider.value <= 6
-        ? "weakest"
-        : lengthSlider.value <= 12
-        ? "weak"
-        : lengthSlider.value <= 18
-        ? "medium"
-        : "strong";
-};
-
-const updateSlider = async () => {
-  document.querySelector(".pass-length span").innerHTML = lengthSlider.value;
 
   if (wordsOption.checked) {
     const numWords = lengthSlider.value;
@@ -109,6 +60,27 @@ const updateSlider = async () => {
     }
     passwordInput.value = randomPassword;
   }
+
+  console.log("passwordInput Length = " + passwordInput.value.length);
+};
+
+
+
+const updatePassIndicator = () => {
+    passIndicator.id =
+        lengthSlider.value <= 6
+        ? "weakest"
+        : lengthSlider.value <= 12
+        ? "weak"
+        : lengthSlider.value <= 18
+        ? "medium"
+        : "strong";
+};
+
+const updateSlider = async () => {
+  passLength.innerHTML = lengthSlider.value;
+
+  generatePassword();
   updatePassIndicator();
 
   console.log("passwordInput Length = " + passwordInput.value.length)
@@ -125,14 +97,19 @@ const copyPassword = () => {
 }
 
 const passInputHeightCheck = () => {
-    
-}
+  if (passwordInput.scrollHeight > passwordInput.clientHeight) {
+    passwordInput.style.height = passwordInput.scrollHeight + "px";
+  }
+};
+
+
 
 copyIcon.addEventListener("click", copyPassword);
 lengthSlider.addEventListener("input", updateSlider);
 generateBtn.addEventListener("click", generatePassword);
-passwordInput.addEventListener("input", function () {
+passwordInput.addEventListener("change", function () {
     passInputHeightCheck();
+    console.log("passwordInput changed...")
 });
 
 options.forEach((option) => {
@@ -143,7 +120,7 @@ options.forEach((option) => {
     }
     option.addEventListener("click", () => {
         if(option.checked){
-
+            
         }else{
             if (!option.checked && document.querySelectorAll(".option input:checked").length === 1 && option.id !== "exc-duplicate") {
                 option.checked = true;
@@ -155,15 +132,15 @@ options.forEach((option) => {
 wordsOption.addEventListener("change", function () {
     if (this.checked) {
         options.forEach(option => {
-        lengthSlider.value = 3
             if (option.id !== "words") {
                 option.checked = false;
                 option.disabled = true;
+                lengthSlider.value = 3
             }
         });
-        console.log("passwordInput length changed =" + passwordInput.value.length)
         passInputHeightCheck()
         updateSlider()
+        console.log("passwordInput length changed =" + passwordInput.value.length)
     }else{
         options.forEach(option => {
             if (option.id !== "words") {
@@ -172,8 +149,41 @@ wordsOption.addEventListener("change", function () {
                 generatePassword();
             }
         });
+        passInputHeightCheck()
+        updateSlider()
     }
 })
 
 updateSlider();
 generatePassword();
+
+
+
+
+// for (const option of options) {
+//     if (option.checked) {
+//       if (option.id === "words") {
+//         const numWords = lengthSlider.value;
+//         const words = await getWords(numWords);
+//         staticPassword += words.join("");
+//         passLength = words.length;
+//       } else if (option.id !== "exc-duplicate") {
+//         staticPassword += characters[option.id];
+//       } else {
+//         excludeDuplicate = true;
+//       }
+//     }
+//   }
+
+//   if (staticPassword !== "") {
+//     for (let i = 0; i < passLength; i++) {
+//       let randomChar =
+//         staticPassword[Math.floor(Math.random() * staticPassword.length)];
+//       if (excludeDuplicate && randomPassword.includes(randomChar)) {
+//         i--;
+//       } else {
+//         randomPassword += randomChar;
+//       }
+//     }
+//     passwordInput.value = randomPassword;
+//   }
